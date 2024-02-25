e02341eed46fef70a28fc2c272381191975a081f
Pak Coding <laolikristian1305@gmail.com>
2024-02-26 01:37:46 +0700

    First Commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React Js</li>
+        <li>React Native</li>
+        <li>Tailwind</li>
+        <li>Bootstrap</li>
+        <li>JavaScript</li>
+        <li>Typescript</li>
+        <li>PosgreSql</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Informatics Engineering</li>
+        <li>Universitas Nasional</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Social Cyber System And Machine Learning</li>
+        <li>Cyber Space Systems For Era Of Society 5.0</li>
+        <li>Data Science & Big Data</li>
+        <li>Programing And Software Development</li>
+        <li>Learn to Create Web Applications with React</li>
+        <li>Learn Web Programming Basics</li>
+        <li>Learn to Create a Web Front-End for Beginners</li>
+        <li>Fullstack Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a dynamic Fullstack Web Developer, specializing in both
+            frontend and backend technologies. Proficient in utilizing Reactjs,
+            React Native, ChakraUI, and Redux, I have successfully engineered
+            innovative and responsive software solutions.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
